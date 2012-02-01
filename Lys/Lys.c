8006ffa79937dae04eae87a58e715310dce42d19
Thomas <Thomas@10.140.32.76>
2012-02-01 10:47:28 +0100

    Red and yellow done!

---
+++ Lys/Lys.c
@@ -0,0 +1,83 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+enum states { GREEN, YELLOW, RED, RED_AND_YELLOW };
+
+enum events { TICK };
+
+int light1 = GREEN;
+int light2 = RED;
+
+void lightcurve1(int event);
+void Do( int State );
+
+//------------------------------
+
+int tickcount;
+
+int main()
+{
+    lightcurve1(TICK);
+
+
+
+    for(tickcount=0;tickcount<2;tickcount++) lightcurve1( TICK );
+
+    return 0;
+}
+
+
+
+void lightcurve1( int event )
+{
+    int NextState = light1;
+
+    switch( light1 )
+    {
+        case GREEN:
+                {
+                   NextState = YELLOW;
+                   break;
+                   default:
+                   break;
+                }
+
+        case YELLOW:
+
+            break;
+            default:
+            break;
+            // The program should never get here !
+    }
+
+    if (NextState != light1)
+     {
+        light1 = NextState;
+    }
+
+    Do( light1 );
+}
+
+
+
+void Do( int State)
+{
+    switch (State) {
+        case GREEN:
+            printf("The light is green\n");
+            break;
+        case YELLOW:
+            printf("The light is yellow.\n");
+            break;
+        case RED:
+            printf("The light is red\n");
+            break;
+        case RED_AND_YELLOW:
+            printf("The light is red and yellow\n");
+            break;
+    }
+}
+
--- Lys/src/Lys.c
+++ Lys/src/Lys.c
@@ -1,78 +1,96 @@
 
-
 #include <stdio.h>
 #include <stdlib.h>
 
+enum states
+{
+  GREEN, YELLOW, RED, RED_AND_YELLOW
+};
 
-enum states { GREEN, YELLOW, RED, RED_AND_YELLOW };
-
-enum events { TICK };
+enum events
+{
+  TICK
+};
 
 int light1 = GREEN;
 int light2 = RED;
 
-void lightcurve1(int event);
-void Do( int State );
+void
+lightcurve1(int event);
+void
+Do(int State);
 
 //------------------------------
 
-int tickcount=0;
+int tickcount;
 
-int main()
+int
+main()
 {
+  lightcurve1(TICK);
+
+  for (tickcount = 0; tickcount < 2; tickcount++)
     lightcurve1(TICK);
 
-
-    //int i;
-    //for(i=0;i<2;i++) lightcurve1( TICK );
-
-    return 0;
+  return 0;
 }
 
-
-
-void lightcurve1( int event )
+void
+lightcurve1(int event)
 {
-    int NextState = light1;
+  int NextState = light1;
 
-    switch( light1 )
+  switch (light1)
     {
-        case GREEN:
-            switch (event )
-            {
-                    NextState = YELLOW;
-            }
-            break;
-            default:
-            break;
-            // The program should never get here !
+  case GREEN:
+    switch (event)
+      {
+    case TICK:
+      NextState = YELLOW;
+      break;
+      }
+    break;
+
+  case YELLOW:
+
+    switch (event)
+      {
+    case TICK:
+      NextState = RED;
+      break;
+      }
+    break;
+  default:
+    break;
+
+    // The program should never get here !
     }
 
-    if (NextState != light1)
-     {
-        light1 = NextState;
+  if (NextState != light1)
+    {
+      light1 = NextState;
     }
 
-    Do( light1 );
+  Do(light1);
 }
 
-
-
-void Do( int State)
+void
+Do(int State)
 {
-    switch (State) {
-        case GREEN:
-            printf("The light is green\n");
-            break;
-        case YELLOW:
-            printf("The light is yellow.\n");
-            break;
-        case RED:
-            printf("The light is red\n");
-            break;
-        case RED_AND_YELLOW:
-            printf("The light is red and yellow\n");
-            break;
+  switch (State)
+    {
+  case GREEN:
+    printf("The light is green\n");
+    break;
+  case YELLOW:
+    printf("The light is yellow.\n");
+    break;
+  case RED:
+    printf("The light is red\n");
+    break;
+  case RED_AND_YELLOW:
+    printf("The light is red and yellow\n");
+    break;
     }
 }
 
